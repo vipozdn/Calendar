8c8bee9d17e6a78f2b64568a959e6420590f8568
Victor Pozdnyakov <victorius152@gmail.com>
2023-05-05 13:11:56 +0600

    Add usedDateTime field to WeekDay
    This field represents the time of the specified date, depending on which the behavior and appearance of the week day can change

--- core/src/main/java/com/kizitonwose/calendar/core/WeekDay.kt
+++ core/src/main/java/com/kizitonwose/calendar/core/WeekDay.kt
@@ -3,12 +3,17 @@ package com.kizitonwose.calendar.core
 import androidx.compose.runtime.Immutable
 import java.io.Serializable
 import java.time.LocalDate
+import java.time.LocalDateTime
 
 /**
  * Represents a day on the week calendar.
  *
  * @param date the date for this day.
  * @param position the [WeekDayPosition] for this day.
+ * @param usedDateTime This field represents the time of the specified date,
+ * depending on which the behavior and appearance of the calendar day can change
  */
 @Immutable
-data class WeekDay(val date: LocalDate, val position: WeekDayPosition) : Serializable
+data class WeekDay(val date: LocalDate,
+                   val position: WeekDayPosition,
+                   val usedDateTime: LocalDateTime) : Serializable
