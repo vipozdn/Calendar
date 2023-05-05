d6ea03083d1ede943ec2a24268402158423ab0bb
Victor Pozdnyakov <victorius152@gmail.com>
2023-05-05 13:09:31 +0600

    Add usedDateTime field to CalendarDay
    This field represents the time of the specified date, depending on which the behavior and appearance of the calendar day can change

--- core/src/main/java/com/kizitonwose/calendar/core/CalendarDay.kt
+++ core/src/main/java/com/kizitonwose/calendar/core/CalendarDay.kt
@@ -3,12 +3,17 @@ package com.kizitonwose.calendar.core
 import androidx.compose.runtime.Immutable
 import java.io.Serializable
 import java.time.LocalDate
+import java.time.LocalDateTime
 
 /**
  * Represents a day on the calendar.
  *
  * @param date the date for this day.
  * @param position the [DayPosition] for this day.
+ * @param usedDateTime This field represents the time of the specified date,
+ * depending on which the behavior and appearance of the calendar day can change
  */
 @Immutable
-data class CalendarDay(val date: LocalDate, val position: DayPosition) : Serializable
+data class CalendarDay(val date: LocalDate,
+                       val position: DayPosition,
+                       val usedDateTime: LocalDateTime) : Serializable
